71acec92f395a12787511c5b9257334c1dcd0473
Lance Edgar <lance@edbob.org>
2024-01-28 17:34:00 -0600

    Add bulma, fontawesome, basic dark mode support

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,56 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <script>
 export default {
+
+    data() {
+        return {
+            darkMode: false,
+        }
+    },
+
     mounted() {
         // nb. for some reason the current route does not load automatically
         // in the context of a published q-app, so we do that explicitly
         this.$router.push(window._qdnPath)
+
+        // set dark mode if applicable
+        if (window._qdnTheme == 'dark') {
+            this.setDarkMode(true)
+        }
+    },
+
+    methods: {
+
+        setDarkMode(dark) {
+            this.darkMode = dark
+            const body = document.querySelector('body')
+            body.className = dark ? 'dark' : ''
+        },
     },
 }
 </script>
 
 <template>
+
   <header>
+
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+
+      <div class="dark-toggle"
+           style="margin-bottom: 15rem;">
+        <a v-if="!darkMode"
+           href="#" @click.prevent="setDarkMode(true)">
+          <o-icon icon="moon" size="large" />
+        </a>
+        <a v-if="darkMode"
+           href="#" @click.prevent="setDarkMode(false)">
+          <o-icon icon="sun" size="large" />
+        </a>
+      </div>
+
+
       <HelloWorld msg="You did it!" />
 
       <nav>
@@ -39,6 +76,17 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+.dark-toggle {
+    margin-bottom: 15rem;
+}
+
+.dark-toggle a:hover {
+    background-color: white;
+}
+body.dark .dark-toggle a:hover {
+    background-color: $dark;
 }
 
 nav {
