53280cb561a57c54535cd88f1a2fa96e24bf64e5
Lance Edgar <lance@edbob.org>
2024-01-28 16:21:28 -0600

    Fix route auto-navigation on app load
    only needed for published q-app, not sure why

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+<script>
+export default {
+    mounted() {
+        // nb. for some reason the current route does not load automatically
+        // in the context of a published q-app, so we do that explicitly
+        this.$router.push(window._qdnPath)
+    },
+}
+</script>
+
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
